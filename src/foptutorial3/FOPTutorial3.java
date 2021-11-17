0471696389410c021ed938a868f1cfc0311a50f2
Faris Faiz <u2000421@siswa.um.edu.my>
2021-11-17 14:22:37 +0800

    FOP Tutorial 3 updated with Q1e

--- src/foptutorial3/FOPTutorial3.java
+++ src/foptutorial3/FOPTutorial3.java
@@ -5,6 +5,7 @@
 package foptutorial3;
 
 import java.util.Scanner;
+import javax.swing.tree.DefaultTreeSelectionModel;
 
 /**
  *
@@ -13,39 +14,72 @@ import java.util.Scanner;
 public class FOPTutorial3 {
 
     public static void main(String[] args) {
+//        Scanner keyboard = new Scanner(System.in);
+//        String temp, strings="";
+//        String[] string_list = new String[99]; 
+//        int string_count=0;
+//    
+//        for(int i=0; i<999; i++){
+//            System.out.print("Enter the string [Quit to quit]: ");
+//            strings = keyboard.nextLine();
+//            switch(strings){
+//                case "Quit":
+//                    i=998;
+//                    break;
+//                default:
+//                    string_list[i] = strings;
+//                    string_count++;
+//                    break;
+//            }
+//        }
+//        
+//        for(int j=0; j<string_count; j++){
+//            for(int k= j + 1; k < string_count; k++){
+//                if(string_list[j].compareTo(string_list[k])>0){
+//                    temp = string_list[j];
+//                    string_list[j] = string_list[k];
+//                    string_list[k] = temp;
+//                }
+//            }
+//        }
+//        
+//        System.out.print("Strings in Sorted Order: ");
+//        for (int i = 0; i <= string_count - 1; i++) 
+//        {
+//            System.out.print(string_list[i] + ", ");
+//        }
+
         Scanner keyboard = new Scanner(System.in);
-        String temp, strings="";
-        String[] string_list = new String[99]; 
-        int string_count=0;
-    
-        for(int i=0; i<999; i++){
-            System.out.print("Enter the string [Quit to quit]: ");
-            strings = keyboard.nextLine();
-            switch(strings){
-                case "Quit":
-                    i=998;
-                    break;
-                default:
-                    string_list[i] = strings;
-                    string_count++;
-                    break;
-            }
-        }
+        String input_user="";
         
-        for(int j=0; j<string_count; j++){
-            for(int k= j + 1; k < string_count; k++){
-                if(string_list[j].compareTo(string_list[k])>0){
-                    temp = string_list[j];
-                    string_list[j] = string_list[k];
-                    string_list[k] = temp;
-                }
-            }
-        }
+        System.out.print("Enter the day: ");
+        input_user = keyboard.nextLine();
         
-        System.out.print("Strings in Sorted Order: ");
-        for (int i = 0; i <= string_count - 1; i++) 
-        {
-            System.out.print(string_list[i] + ", ");
+        switch(input_user){
+            case "Sunday":
+                System.out.println("Sunday - 0");
+                break;
+            case "Monday":
+                System.out.println("Monday - 1");
+                break;
+            case "Tuesday":
+                System.out.println("Tuesday - 2");
+                break;
+            case "Wednesday":
+                System.out.println("Wednesday - 3");
+                break;
+            case "Thursday":
+                System.out.println("Thursday - 4");
+                break;
+            case "Friday":
+                System.out.println("Friday - 5");
+                break;
+            case "Saturday":
+                System.out.println("Saturday - 6");
+                break;
+            default:
+                System.out.println("Error, invalid");
+                break;
         }
     }
     
